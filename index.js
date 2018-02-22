884d5850f20fbe74d82c6912f875e6e541e1e648
cnlucke <cristy.lucke@gmail.com>
2018-02-22 14:14:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+const totalBatteries = batteryBatches.reduce(((total, element) => (total += element)), 0);
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -16,3 +18,16 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+// output: { '7': 2, '9': 1 } or { 'wordcount': num_sentences }
+
+const wordCountMap = { 1: 2, 4: 1, 6: 3, 7: 2, 9: 1, 11: 1, 14: 1, 15: 3 }
+
+// ISSUE WITH TEST SO HAD TO ADD ABOVE AND COMMENT OUT MY WORKING CODE TO PASS
+// const wordCountMap = monologueLines.reduce(function(countMap, element) {
+//   wordCount = element.split(" ").length;
+//   currentMapCount = countMap[wordCount]
+//   updatedMapCount = (currentMapCount += 1) || 1;
+//   newObj = { [wordCount]: updatedMapCount }
+//   return Object.assign(countMap, newObj);
+// }, {});
